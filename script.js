63c4283ce89831956b239b189aeb2ef7e04dc4b4
Teejay012 <106912602+Teejay012@users.noreply.github.com>
2023-10-04 03:39:10 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -3,7 +3,8 @@
 const headerMenuIcon = document.querySelector('.header__menu__icon i'),
 mobileHeaderNav = document.querySelector('.mobile__header__nav'),
 header = document.querySelector('.header'),
-aboutHeader = document.querySelector('.about__header');
+aboutHeader = document.querySelector('.about__header'),
+serviceHeader = document.querySelector('.service__header');
 
 headerMenuIcon.addEventListener("click", ()=>{
     if(headerMenuIcon.classList.contains('bx-menu-alt-right')){
@@ -23,9 +24,11 @@ window.addEventListener('scroll', ()=>{
     let windowScroll = window.scrollY;
     if (windowScroll > 20){
         header.style.boxShadow = '0 0 15px rgba(0, 0, 0, 0.2)';
-        aboutHeader.style.background = 'var(--first-color)'
+        aboutHeader.style.background = 'var(--first-color)';
+        serviceHeader.style.background = 'var(--first-color)';
     }else{
         header.style.boxShadow = 'none';
-        aboutHeader.style.background = 'transparent'
+        aboutHeader.style.background = 'transparent';
+        serviceHeader.style.background = 'transparent';
     }
 })
